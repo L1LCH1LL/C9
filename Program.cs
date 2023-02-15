6597b762a730c5a44ce702b81b6a24bb5d6e21c3
L1LCH1LL <108305081+L1LCH1LL@users.noreply.github.com>
2023-02-15 04:12:43 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
-﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все
-натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
-N = 5 -> "5, 4, 3, 2, 1"
-N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
 */
 
 
-Console.WriteLine("Введите натуральное число больше 1:");
-int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите начальное число M:");
+int numM = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите начальное число M:");
+int numN = int.Parse(Console.ReadLine());
 
-void NumberCounter (int number)
+void PrSum (int numM, int numN, int sum)
 {
-    if (number < 0) Console.Write($"{number} не натуральное число");
-    if (number == 0) return;
-    Console.Write(number + "  " );
-    NumberCounter (number - 1);
+    if (numM > numN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numM++);
+    PrSum(numM, numN, sum);
 }
 
-NumberCounter(number);
+PrSum(numM, numN, 0);
